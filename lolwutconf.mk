e9fababcac0c5bed3005759e99031cf0d6c867d3
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2016-09-24 00:05:20 +0800

    _Really_ properly support separate source and build directories.
    configure.inc :
    - now determines $conf_Srcdir based on location of $0
    
    lolwutconf.mk :
    - now adds two `-I...' flags to $(CPPFLAGS) and $(CPPFLAGS_
      FOR_TARGET) if source and build directories differ

--- lolwutconf.mk
+++ lolwutconf.mk
@@ -1,7 +1,13 @@
 VPATH = $(conf_Srcdir)
 
-CPPFLAGS := -I$(conf_Srcdir)/include $(CPPFLAGS)
-CPPFLAGS_FOR_TARGET := -I$(conf_Srcdir)/include $(CPPFLAGS_FOR_TARGET)
+ifeq "$(abspath $(conf_Srcdir))" "$(abspath .)"
+    CPPFLAGS := -Iinclude $(CPPFLAGS)
+    CPPFLAGS_FOR_TARGET := -Iinclude $(CPPFLAGS_FOR_TARGET)
+else
+    CPPFLAGS := -Iinclude -I$(conf_Srcdir)/include $(CPPFLAGS)
+    CPPFLAGS_FOR_TARGET := -Iinclude -I$(conf_Srcdir)/include \
+	$(CPPFLAGS_FOR_TARGET)
+endif
 
 conf_Prefix ?= /usr/local
 ifeq "." "$(conf_Srcdir)"
