e38a51a4e0cbd66456e6f429e30d650c42c0bad4
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2017-08-20 21:08:13 +0800

    "The adjective is the banana peel of the parts of speech."
    dosemu-wrapper-1.sh :
    - may now pass `--Flibdir ...' to dosemu.bin, to handle case
      where dosemu is installed away from its original destination

--- dosemu-wrapper-1.sh
+++ dosemu-wrapper-1.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-exec dosemu.bin -q -I 'video {none}' ${1+"$@"}
+libdir="`which dosemu.bin | xargs dirname`"/../share/dosemu
+if test -f "$libdir"/commands/dosemu/exitemu.com; then
+	exec dosemu.bin -q --Flibdir "$libdir" -I 'video {none}' ${1+"$@"}
+else
+	exec dosemu.bin -q -I 'video {none}' ${1+"$@"}
+fi
