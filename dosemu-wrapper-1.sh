1ae66a2f12567faffd79e0cdfa06645846723c94
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2018-03-04 00:57:20 +0800

    "If there are epigrams, there must be meta-epigrams."
    dosemu-wrapper-1.sh :
    - now explicitly specifies -K to dosemu.bin

--- dosemu-wrapper-1.sh
+++ dosemu-wrapper-1.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 libdir="`which dosemu.bin | xargs dirname`"/../share/dosemu
 if test -f "$libdir"/commands/dosemu/exitemu.com; then
-	exec dosemu.bin -q --Flibdir "$libdir" -I 'video {none}' ${1+"$@"}
+	exec dosemu.bin -q --Flibdir "$libdir" -I 'video {none}' -K ${1+"$@"}
 else
-	exec dosemu.bin -q -I 'video {none}' ${1+"$@"}
+	exec dosemu.bin -q -I 'video {none}' -K ${1+"$@"}
 fi
