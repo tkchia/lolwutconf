47bb66cddba37adf31616bae8d6768118b06b747
Tee-Kiah Chia <tkchia@users.noreply.github.com>
2018-11-12 23:10:45 +0800

    "It may be slow, but it's hard to use."  (2)
    dosemu-wrapper-1.sh :
    - now passes `keyboard {layout us}' option to dosemu
    - now connects stdin and stderr to /dev/null
    - now uses dosemu's -p option rather than using sed
    
    dosemu-wrapper-2.sh :
    - now passes `keyboard {layout us}' option to dosemu
    - now connects stdin and stderr to /dev/null

--- dosemu-wrapper-1.sh
+++ dosemu-wrapper-1.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 libdir="`which dosemu.bin | xargs dirname`"/../share/dosemu
 if test -f "$libdir"/commands/dosemu/exitemu.com; then
-	dosemu.bin --Flibdir "$libdir" -I 'video {none}' -K ${1+"$@"} | \
-	    sed '1,/Execute : / ! p'
+	dosemu.bin --Flibdir "$libdir" \
+	    -I 'video {none} keyboard {layout us}' -p -K ${1+"$@"} \
+	    </dev/null 2>/dev/null
 else
-	dosemu.bin -I 'video {none}' -K ${1+"$@"} | sed '1,/Execute : / ! p'
+	dosemu.bin -I 'video {none} keyboard {layout us}' -p -K ${1+"$@"} \
+	    </dev/null 2>/dev/null
 fi
--- dosemu-wrapper-2.sh
+++ dosemu-wrapper-2.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-exec dosemu.bin -I 'cpuemu vm86sim video {none}' ${1+"$@"}
+exec dosemu.bin -I 'cpuemu vm86sim video {none} keyboard {layout us}' \
+    ${1+"$@"} </dev/null 2>/dev/null
